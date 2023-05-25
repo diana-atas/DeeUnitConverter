234da5dd54ae9eb0fcb3b50fb1fe1501e3209f2b
Diana Atas <diana.atas@trademe.co.nz>
2023-05-26 11:32:51 +1200

    Initial Commit

---
+++ DeeUnitConverter/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  DeeUnitConverter
+//
+//  Created by Diana Atas on 26/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DeeUnitConverter/DeeUnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeeUnitConverterApp.swift
+//  DeeUnitConverter
+//
+//  Created by Diana Atas on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DeeUnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
